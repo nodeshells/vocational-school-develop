b9cfc91c39eb4a9448cb060a22a9aa3c9bc335e2
MEANスタック大好きおじさん <kenta.job09@gmail.com>
2018-07-26 12:31:14 +0900

    消した

--- recommend/server/config.ts
+++ recommend/server/config.ts
@@ -15,9 +15,9 @@ export const MONGO_URL_SESSION = 'mongodb://150.95.148.134:28001/sessiondata';
 // googleAPIkey
 export const G_USER = 'Recommend911@gmail.com';
 export const G_PASS = 'firestack';
-export const CLIENT_ID = '945735306455-dqvk4usecssqg0gou4j662lpmd84iipg.apps.googleusercontent.com';
-export const CLIENT_SEC = 'oeNERGqeNFZR24OyGmRVx5WS ';
-export const REFRESH_TOKEN = '1/FTYsEbvr62sUh5g1tOqsnbM4JNsIlgSUZGP5wp2jhlY';
+export const CLIENT_ID = 'gou4j662lpmd84iipg.apps.googleusercontent.com';
+export const CLIENT_SEC = 'oyGmRVx5WS ';
+export const REFRESH_TOKEN = 'snbM4JNsIlgSUZGP5wp2jhlY';
 
 // メールの設定
 export const FROM = 'Recommend運営<Recommend@gmail.com>';
